917ad63fb7877e9eecd2fa77100731989bd7fffe
Morne <morne2018@gmail.com>
2019-03-29 23:18:37 +0200

    Added launch screen and icon

--- app/src/main/java/com/itsp/attendance/MainActivity.java
+++ app/src/main/java/com/itsp/attendance/MainActivity.java
@@ -2,11 +2,13 @@ package com.itsp.attendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
