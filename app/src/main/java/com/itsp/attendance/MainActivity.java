9969d35ddfdc28b9982b61042d3dab552f4112d5
Morne <morne2018@gmail.com>
2019-06-17 17:09:02 +0200

    Cleaned up MainActivity class

--- app/src/main/java/com/itsp/attendance/MainActivity.java
+++ app/src/main/java/com/itsp/attendance/MainActivity.java
@@ -2,25 +2,32 @@ package com.itsp.attendance;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.itsp.attendance.barcodereader.BarcodeCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+    private static final int RC_BARCODE_CAPTURE = 9001;
+
     HomeFragment homeFragment;
     SubjectFragment subjectFragment;
     NotificationFragment notificationFragment;
 
     FloatingActionButton qrReaderButton;
 
-    private static final int RC_BARCODE_CAPTURE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,30 +39,22 @@ public class MainActivity extends AppCompatActivity {
         notificationFragment = new NotificationFragment();
 
         // NOTE(Morne): Sets the initial fragment to the home_fragment.
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, homeFragment);
-        fragmentTransaction.commit();
-
+        switchFragment(homeFragment);
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
 
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                 switch (item.getItemId()) {
-
                     case R.id.navigation_home:
-                        fragmentTransaction.replace(R.id.fragment_container, homeFragment);
-                        fragmentTransaction.commit();
+                        switchFragment(homeFragment);
                         return true;
                     case R.id.navigation_subject:
-                        fragmentTransaction.replace(R.id.fragment_container, subjectFragment);
-                        fragmentTransaction.commit();
+                        switchFragment(subjectFragment);
                         return true;
                     case R.id.navigation_notification:
-                        fragmentTransaction.replace(R.id.fragment_container, notificationFragment);
-                        fragmentTransaction.commit();
+                        switchFragment(notificationFragment);
                         return true;
                 }
                 return false;
@@ -74,5 +73,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Barcode result
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    Toast.makeText(MainActivity.this, barcode.displayValue, Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Barcode read: " + barcode.displayValue);
+                } else {
+                    Log.d(TAG, "No barcode captured, intent data is null");
+                }
+            } else {
+                // statusMessage.setText(String.format(getString(R.string.barcode_error),
+                // CommonStatusCodes.getStatusCodeString(resultCode)));
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 
+    private void switchFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
 }
